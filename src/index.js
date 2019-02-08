de0575fe13ddc7675192b92aeac66408573fcd08
xgyuyu <409816503@qq.com>
2019-02-08 21:32:40 +0800

    完成一部分

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './css/index.less'
+import './css/index.css'
+
+ReactDOM.render(
+  <div>Hello World123</div>, document.getElementById('app')
+)
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+ReactDOM.render(
+    <div>Hello World123</div>, document.getElementById('app')
+)
